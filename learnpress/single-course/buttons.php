3b5da2bbcb96bb1fae6f5a547a63aae76e325e1a
kyohei <tonton.k.pg@gmail.com>
2021-09-10 17:23:14 +0900

    temp

--- learnpress/single-course/buttons.php
+++ learnpress/single-course/buttons.php
@@ -31,24 +31,27 @@ defined('ABSPATH') || exit();
         <?php if ($is_logged_in_and_paid): // ログイン課金者?>
             <p>課金者です</p>
 			<?php
-			do_action('learn-press/before-course-buttons');
+            do_action('learn-press/before-course-buttons');
 
-			/**
-			 * @see LP_Template_Course::course_purchase_button - 10
-			 * @see LP_Template_Course::course_enroll_button - 10
-			 * @see learn_press_course_retake_button - 10
-			 */
-			do_action('learn-press/course-buttons');
+            /**
+             * @see LP_Template_Course::course_purchase_button - 10
+             * @see LP_Template_Course::course_enroll_button - 10
+             * @see learn_press_course_retake_button - 10
+             */
+            do_action('learn-press/course-buttons');
 
-			do_action('learn-press/after-course-buttons');
-			?>
+            do_action('learn-press/after-course-buttons');
+            ?>
         <?php elseif ($is_logged_in_not_paid): // ログイン無課金?>
             <p>無課金バーサーク！！</p>
 			<div id="learn-press-pmpro-notice" class="learn-press-pmpro-buy-membership purchase-course">
-				<a class="lp-button button purchase-button" href="https://xs264882.xsrv.jp/lambda/%E3%83%A1%E3%83%B3%E3%83%90%E3%83%BC%E3%82%B7%E3%83%83%E3%83%97%E3%82%A2%E3%82%AB%E3%82%A6%E3%83%B3%E3%83%88/%E4%BC%9A%E5%93%A1%E3%83%AC%E3%83%99%E3%83%AB/">Buy Membership</a>
+				<a class="lp-button button purchase-button" href="/lambda/%E3%83%A1%E3%83%B3%E3%83%90%E3%83%BC%E3%82%B7%E3%83%83%E3%83%97%E3%82%A2%E3%82%AB%E3%82%A6%E3%83%B3%E3%83%88/%E4%BC%9A%E5%93%A1%E3%83%AC%E3%83%99%E3%83%AB/">Buy Membership</a>
 			</div>
         <?php else: // ログインしていない?>
             <p>ログインしてよね！</p>
+			<div id="learn-press-pmpro-notice" class="learn-press-pmpro-buy-membership purchase-course">
+				<a class="lp-button button purchase-button" href="/lambda/user-profile/">Buy Membership</a>
+			</div>
         <?php endif; ?>
 
 
--- page-login-user-only-page.php
+++ page-login-user-only-page.php
@@ -5,5 +5,5 @@
 
 if (is_user_logged_in()) {
 } else {
-    wp_redirect('lambda/user-profile/');
+    wp_redirect('/lambda/user-profile/');
 }
